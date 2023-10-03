c7273fddeeb6cd953f420d8f0d6be19f291889fc
SYSNARA <darbingallardo9@gmail.com>
2023-10-02 23:18:12 -0600

    FormEncargado

--- src/app/docentes/docentes.component.ts
+++ src/app/docentes/docentes.component.ts
@@ -10,4 +10,9 @@ export class DocentesComponent {
   RegistrarDocentes(): void{
 
   }
+
+  listaEncargado: any[] = [ /*Listas en la table*/
+  {year: '2022', FechaInicial: '2022-02-01', FechaFinal: '2022-11-30', Observacion: 'NINGUNA'},
+  {year: '2021', FechaInicial: '2021-02-01', FechaFinal: '2021-11-30', Observacion: 'NINGUNA'},
+];
 }
